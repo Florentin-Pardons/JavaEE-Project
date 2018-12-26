9652bbc74df4e79943eb32a21c3b775ca7d70ced
Florentin <Flo@gmail.com>
2018-12-26 11:46:08 +0100

    Create Reservation.java
    Creation de la Class Reservation

---
+++ JAVAEE-Projet/API_REST_Voiture_Projet/src/javabean/Reservation.java
@@ -0,0 +1,41 @@
+package javabean;
+
+import java.util.Date;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name="reservation")
+public class Reservation {
+
+	//Variable
+	private Date dateRes;
+	private Voiture voiture;
+	
+	//Getter et Setteur
+	@XmlElement(name="dateres_reservation")
+	public Date getDateRes() {
+		return dateRes;
+	}
+	
+	public void setDateRes(Date dateRes) {
+		this.dateRes = dateRes;
+	}
+	
+	@XmlElement(name="voiture_reservation")
+	public Voiture getVoiture() {
+		return voiture;
+	}
+	
+	public void setVoiture(Voiture voiture) {
+		this.voiture = voiture;
+	}
+	
+	//Constructeur
+	public Reservation() {}
+	
+	public Reservation(Date dateRes, Voiture voiture) {
+		this.dateRes = dateRes;
+		this.voiture = voiture;
+	}
+}
