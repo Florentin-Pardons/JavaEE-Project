072dda176fbbc6c8c431cea90fa6a505f96d8cc4
Florentin <Flo@gmail.com>
2019-01-03 13:50:21 +0100

    Update Utilisateur_Rest.java

--- JAVAEE-Projet/API_REST_Voiture_Projet/src/api_rest/Utilisateur_Rest.java
+++ JAVAEE-Projet/API_REST_Voiture_Projet/src/api_rest/Utilisateur_Rest.java
@@ -130,7 +130,7 @@ public class Utilisateur_Rest {
     
 	//Get
 	@GET
-    @Produces(MediaType.APPLICATION_XML)
+    @Produces(MediaType.APPLICATION_JSON)
     public Response getUtilisateur(@QueryParam("id") int id)
 	{
 		Connection connec = OracleConnexion.getInstance();
