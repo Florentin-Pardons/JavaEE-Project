0b5a8c8eb1562798b526c336992b7acddbe23b80
Florentin <Flo@gmail.com>
2019-01-03 14:24:27 +0100

    Create Modele.java

---
+++ JAVAEE-Projet/Client_Voiture_Projet/src/javabean/Modele.java
@@ -0,0 +1,107 @@
+package javabean;
+
+import java.io.IOException;
+import java.util.List;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+
+import dao.Modele_DAO;
+
+public class Modele {
+
+	//Variable
+	private int id;
+	private String nom;
+	private int nbPorte;
+	private int volumeCoffre;
+	private Marque marque;
+	private Categorie categorie;
+	
+	//Getter et Setteur
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	public int getNbPorte() {
+		return nbPorte;
+	}
+	
+	public void setNbPorte(int nbPorte) {
+		this.nbPorte = nbPorte;
+	}
+	
+	public int getVolumeCoffre() {
+		return volumeCoffre;
+	}
+	
+	public void setVolumeCoffre(int volumeCoffre) {
+		this.volumeCoffre = volumeCoffre;
+	}
+	
+	public Marque getMarque() {
+		return marque;
+	}
+
+	public void setMarque(Marque marque) {
+		this.marque = marque;
+	}
+
+	public Categorie getCategorie() {
+		return categorie;
+	}
+
+	public void setCategorie(Categorie categorie) {
+		this.categorie = categorie;
+	}
+	
+	//Constructeur
+	public Modele() {}
+	
+	public Modele(String nom, int nbPorte, int volumeCoffre, Marque marque, Categorie categorie)
+	{
+		this.nom = nom;
+		this.nbPorte = nbPorte;
+		this.volumeCoffre = volumeCoffre;
+		this.marque = marque;
+		this.categorie = categorie;
+	}
+	
+	//Methode
+	//Creer
+	public boolean Creer() throws JsonParseException, JsonMappingException, IOException {
+		Modele_DAO mod = new Modele_DAO();
+		return mod.create(this);
+	}
+	
+	//Delete
+	public boolean Delete() throws JsonParseException, JsonMappingException, IOException {
+		Modele_DAO mod = new Modele_DAO();
+		return mod.delete(this);
+	}
+	
+	//Update
+	public boolean Update() throws JsonParseException, JsonMappingException, IOException {
+		Modele_DAO mod = new Modele_DAO();
+		return mod.update(this);
+	}
+		
+	//Creation de la liste
+	public static List<Modele> List() throws JsonParseException, JsonMappingException, IOException
+	{
+		Modele_DAO mod = new Modele_DAO();
+		return mod.list();
+	}
+}
