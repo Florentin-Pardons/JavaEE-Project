1bc89ccbb2262df3f32319ef0c59cae854ac7972
Florentin <Flo@gmail.com>
2019-01-04 22:32:18 +0100

    Create Profil.java

---
+++ JAVAEE-Projet/Client_Voiture_Projet/src/servlets/Profil.java
@@ -0,0 +1,42 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+@WebServlet("/Profil")
+public class Profil extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+    public Profil() {
+    	
+    }
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		HttpSession session=request.getSession(false); 
+		if(session == null)
+		{
+			request.setAttribute("titre", "Login");
+			getServletContext().getRequestDispatcher("/Vues/Compte\\login.jsp").forward(request, response);
+		}
+		else
+		{
+			request.setAttribute("titre", "Profil");
+			getServletContext().getRequestDispatcher("/Vues/Compte\\profil.jsp").forward(request, response);
+		} 
+		
+		
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
